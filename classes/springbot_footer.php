9ad3848e1e74bd44cc1ff0264c07ef2725b3eb4f
Evan Jacobs <evan.jacobs2@gmail.com>
2018-08-23 00:21:37 -0400

    Webhooks and footer tracking fixes

--- classes/springbot_footer.php
+++ classes/springbot_footer.php
@@ -8,7 +8,7 @@ if ( ! class_exists( 'Springbot_Footer' ) ) {
 		 * Show the async script only if the GUID is set
 		 */
 		public function show_async_script() {
-			global $product;
+			global $product, $wp;
 
 			if ( $guid = $this->get_guid() ) {
 
@@ -19,7 +19,7 @@ if ( ! class_exists( 'Springbot_Footer' ) ) {
 				echo "   var fs = document.getElementsByTagName('script')[0];\n";
 				echo "   sb.type = 'text/javascript';\n";
 				echo "   sb.async = true;\n";
-				echo "   sb.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + " . SPRINGBOT_ASSETS_DOMAIN . "/async/preload/{$guid}.js\n";
+				echo "   sb.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + '" . SPRINGBOT_ASSETS_DOMAIN . "/async/preload/{$guid}.js'\n";
 				echo "   fs.parentNode.insertBefore(sb, fs);\n";
 				echo "  })();\n";
 				echo "</script>\n";
@@ -30,6 +30,20 @@ if ( ! class_exists( 'Springbot_Footer' ) ) {
 					     . "&pageurl=" . urlencode( $product->get_permalink() )
 					     . "&sku=" . $product->get_sku() . "\""
 					     . "style=\"position:absolute; visibility:hidden\">\n";
+				}
+
+				// Load AdRoll conversion tracking on checkout success (aka order received) page
+				if ( is_order_received_page() ) {
+					$order_id = isset( $wp->query_vars['order-received'] ) ? intval( $wp->query_vars['order-received'] ) : null;
+					if ( $order_id ) {
+						$order = new WC_Order( $order_id );
+						if ( $order instanceof WC_Order ) {
+							echo "<script type=\"text/javascript\">\n";
+							echo "  adroll_conversion_value = {$order->get_total()};\n";
+							echo "  adroll_currency = \"{$order->get_currency()}\";\n";
+							echo "</script>\n";
+						}
+					}
 				}
 			}
 		}
