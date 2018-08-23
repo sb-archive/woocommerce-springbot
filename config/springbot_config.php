0c34001f70b14c4397fbcc056b73c9aebb324bb3
Evan Jacobs <evan.jacobs2@gmail.com>
2018-08-23 10:24:34 -0400

    Remove scheme from assets domain

--- config/springbot_config.php
+++ config/springbot_config.php
@@ -7,5 +7,5 @@ define( 'SPRINGBOT_REQUIRED_PHP_VERSION', '5.3' );
 define( 'SPRINGBOT_REQUIRED_WP_VERSION', '3.1' );
 
 define( 'SPRINGBOT_WOO_ETL', 'https://woo-etl-api-prod.herokuapp.com' );
-define( 'SPRINGBOT_ASSETS_DOMAIN', 'https://d2z0bn1jv8xwtk.cloudfront.net' );
+define( 'SPRINGBOT_ASSETS_DOMAIN', 'd2z0bn1jv8xwtk.cloudfront.net' );
 define( 'SPRINGBOT_APP_URL', 'https://app.springbot.com' );
