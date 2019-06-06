3f7ae6e1535a211cbee9ed29162b2ee7c6e2bb5a
Paul Dewey <paul.dewey@outlook.com>
2019-06-06 19:07:29 +0000

    Adds database deletion calls upon uninstall.

--- uninstall.php
+++ uninstall.php
@@ -4,5 +4,8 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 }
 
 if ( $user = get_user_by( 'login', 'springbot' ) ) {
+    delete_user_meta( $user->ID, 'springbot_security_token' );
+    delete_user_meta( $user->ID, 'springbot_store_guid' );
+    delete_user_meta( $user->ID, 'springbot_store_id' );
 	return wp_delete_user( $user->ID );
 }
