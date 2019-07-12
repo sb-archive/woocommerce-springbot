73d2e587af85a1f5fa00c8f6b9dadad5241a0aaa
Paul Dewey <paul.dewey@outlook.com>
2019-07-12 11:57:15 +0000

    Polishes up uninstall etl call.

--- uninstall.php
+++ uninstall.php
@@ -3,9 +3,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
-$uninstall_url = SPRINGBOT_WOO_ETL . '/woocommerce/destroy';
-
-function remove_springbot_plugin() {
+function remove_springbot_database_entries() {
     if ( is_admin() ) {
 
         remove_action( 'admin_menu', array( $GLOBALS["woocommerce_springbot"], 'springbot_menu_page' ) );
@@ -35,51 +33,33 @@ function remove_springbot_plugin() {
     }
 }
 
-remove_springbot_plugin();
+public function remove_springbot_etl_entry() {
+    $store_id = -1;
+    if ( $user = get_user_by( 'login', 'springbot' ) ) {
+        $store_id = get_user_meta($user->ID, 'springbot_store_id', true);
+    }
 
-// $response = wp_remote_post( $registration_url, array(
-//     'method'      => 'POST',
-//     'timeout'     => 45,
-//     'redirection' => 5,
-//     'httpversion' => '1.0',
-//     'blocking'    => true,
-//     'headers'     => array( 'Content-Type' => 'application/json; charset=utf-8' ),
-//     'body'        => json_encode( array(
-//         'stores'              => array(
-//             array(
-//                 'local_store_id'         => 1,
-//                 'instance_id'            => null,
-//                 'guid'                   => $this->generate_guid(),
-//                 'name'                   => get_bloginfo( 'name' ),
-//                 'code'                   => $this->normalize( get_bloginfo( 'name' ) ),
-//                 'url'                    => $store_url,
-//                 'enabled'                => true,
-//                 'secure_url'             => $store_url,
-//                 'media_url'              => $store_url,
-//                 'web_id'                 => 1,
-//                 'store_mail_address'     => is_string( get_option( 'woocommerce_store_address' ) ) ? get_option( 'woocommerce_store_address' ) : '',
-//                 'customer_service_email' => get_bloginfo( 'admin_email' ),
-//                 'logo_url'               => '',
-//                 'logo_alt_tag'           => get_bloginfo( 'name' ),
-//                 'store_statuses'         => array(
-//                     'pending-payment' => 'Pending Payment',
-//                     'failed'          => 'Failed',
-//                     'processing'      => 'Processing',
-//                     'completed'       => 'Completed',
-//                     'on-hold'         => 'On-Hold',
-//                     'cancelled'       => 'Cancelled',
-//                     'refunded'        => 'Refunded'
-//                 )
-//             )
-//         ),
-//         'platform'            => 'woocommerce',
-//         'plugin_version'      => SPRINGBOT_PLUGIN_VERSION,
-//         'woocommerce_version' => WC()->version,
-//         'consumer_key'        => $consumer_key,
-//         'consumer_secret'     => $consumer_secret,
-//         'credentials'         => array(
-//             'user_id'  => $email,
-//             'password' => $password
-//         )
-//     ) ),
-// ) );
+    $uninstall_url = SPRINGBOT_WOO_ETL . '/woocommerce/destroy';
+    $response = wp_remote_post( $uninstall_url, array(
+        'method'      => 'POST',
+        'timeout'     => 45,
+        'redirection' => 5,
+        'httpversion' => '1.0',
+        'blocking'    => false,
+        'headers'     => array( 'Content-Type' => 'application/json; charset=utf-8' ),
+        'body'        => json_encode(
+            'store_id' => $store_id
+        ) ),
+    ) ;
+
+    if ( is_wp_error( $response ) ) {
+        $error_message = $response->get_error_message();
+        error_log( "Error during Springbot uninstall: {$error_message}" );
+
+        return 500;
+    } else {
+        return $response['response']['code'];
+    }
+}
+
+remove_springbot_database_entries();
