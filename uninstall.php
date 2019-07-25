8719ade2982a68c6ae302dbc2f1b2b05b84510ca
Paul Dewey <paul.dewey@outlook.com>
2019-07-25 18:48:38 +0000

    Adds function to revoke springbot api keys.

--- uninstall.php
+++ uninstall.php
@@ -12,4 +12,13 @@ function remove_springbot_database_entries() {
     }
 }
 
+function revoke_springbot_api_keys() {
+    global $wpdb;
+    $user_id = $wpdb->get_row("SELECT * FROM ".$wpdb->prefix."users WHERE user_login = 'springbot'", ARRAY_A)['ID'];
+    if (isset($user_id)) {
+        $wpdb->delete($wpdb->prefix."woocommerce_api_keys", ['user_id' => $user_id], '%d');
+    }
+}
+
+revoke_springbot_api_keys();
 remove_springbot_database_entries();
