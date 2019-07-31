2b500015174db6f3f548a9584afaf7c95aad1c9c
Paul Dewey <paul.dewey@outlook.com>
2019-07-31 17:35:25 +0000

    Removes user, guid, and store id database deletions.

--- uninstall.php
+++ uninstall.php
@@ -6,9 +6,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 function remove_springbot_database_entries() {
     if ( $user = get_user_by( 'login', 'springbot' ) ) {
         delete_user_meta( $user->ID, 'springbot_security_token' );
-        delete_user_meta( $user->ID, 'springbot_store_guid' );
-        delete_user_meta( $user->ID, 'springbot_store_id' );
-        wp_delete_user( $user->ID );
     }
 }
 
