993ea63f026e17a0d9d04955f6b5029167d81ecd
Joey Kimsey <joeyk4816@gmail.com>
2019-10-30 15:44:02 -0400

    version increase

--- woocommerce-springbot.php
+++ woocommerce-springbot.php
@@ -3,7 +3,7 @@
  * Plugin Name: Springbot WooCommerce Integration
  * Plugin URI: https://www.springbot.com/
  * Description: Integration plugin between WooCommerce and Springbot
- * Version: 0.0.14
+ * Version: 0.0.15
  * Author: Springbot
  *
  * @package Woocommerce_Springbot
